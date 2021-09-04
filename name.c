70f54c2f995ccb9db9b8dd96aeaa8d2c0bc62ba0
BenjaminCloverWilson <89162179+BenjaminCloverWilson@users.noreply.github.com>
2021-09-03 18:25:01 -0600

    Add files via upload

---
+++ name.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void){
+    
+    printf("BBBBBBBBBBBBBBBBBBB\n");
+    printf("BBB              BBB\n");
+    printf("BBB               BBB\n");
+    printf("BBB                 BBB\n");
+    printf("BBB                 BBB\n");
+    printf("BBB                 BBB\n");
+    printf("BBB               BBB           eeeeeeeeee          nnnn     nnnnnnn\n");
+    printf("BBB              BBB         eeeee      eeeee       nnnn   nnnn    nnnn\n");
+    printf("BBBBBBBBBBBBBBBBBBB         eeeee         eeeee     nnnn nnnn        nnnn\n");
+    printf("BBB              BBB        eeeee         eeeeee    nnnnnnnn          nnnn\n");
+    printf("BBB               BBB       eeeeeeeeeeeeeeeeeeee    nnnnnn            nnnn\n");
+    printf("BBB                 BBB     eeeeeeeeeeeeeeeeeee     nnnnn             nnnn\n");
+    printf("BBB                  BBB    eeeeee                  nnnn              nnnn\n");
+    printf("BBB                  BBB    eeeeee                  nnnn              nnnn\n");
+    printf("BBB                  BBB     eeeeee                 nnnn              nnnn\n");
+    printf("BBB                 BBB      eeeeeeeeee             nnnn              nnnn\n");
+    printf("BBB                BBB         eeeeeeeeeeeeee       nnnn              nnnn\n");
+    printf("BBBBBBBBBBBBBBBBBBBBB            eeeeeeeeeeeee      nnnn              nnnn\n");
+    
+    
+    return 0;
+}
